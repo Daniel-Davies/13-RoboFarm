d9c6d5ec747c37e8801c0d4c5e89bd9221b5a7db
Daniel-Davies <dd16785@bristol.ac.uk>
2019-05-07 19:12:48 -0700

    added agent that plants seeds

---
+++ Code/Simple.py
@@ -0,0 +1,186 @@
+
+from __future__ import print_function
+# ------------------------------------------------------------------------------------------------
+# Copyright (c) 2016 Microsoft Corporation
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+# associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# ------------------------------------------------------------------------------------------------
+
+# Tutorial sample #2: Run simple mission using raw XML
+
+from builtins import range
+import MalmoPython
+import os
+import sys
+import time
+
+if sys.version_info[0] == 2:
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+else:
+    import functools
+    print = functools.partial(print, flush=True)
+
+# More interesting generator string: "3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"
+
+num_seeds = 10
+
+missionXML = '''<?xml version="1.0" encoding="UTF-8" ?>
+    <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <About>
+            <Summary></Summary>
+        </About>
+
+        <ModSettings>
+            <MsPerTick>100</MsPerTick>
+        </ModSettings>
+
+        <ServerSection>
+            <ServerInitialConditions>
+                <Time>
+                    <StartTime>6000</StartTime>
+                    <AllowPassageOfTime>false</AllowPassageOfTime>
+                </Time>
+                <Weather>clear</Weather>
+                <AllowSpawning>false</AllowSpawning>
+            </ServerInitialConditions>
+            <ServerHandlers>
+                <FlatWorldGenerator generatorString="3;7,220*1,5*3,2;3;,biome_1" />
+                <DrawingDecorator>
+                    <DrawCuboid x1="-50" y1="226" z1="-50" x2="50" y2="228" z2="50" type="air" />
+                    <DrawCuboid x1="-50" y1="226" z1="-50" x2="50" y2="226" z2="50" type="water" />
+                    <DrawCuboid x1="-3" y1="226" z1="-3" x2="3" y2="226" z2="3" type="farmland" />
+                    <DrawCuboid x1="-1" y1="226" z1="-1" x2="1" y2="226" z2="1" type="stone" />
+                </DrawingDecorator>
+                <ServerQuitWhenAnyAgentFinishes />
+            </ServerHandlers>
+        </ServerSection>
+
+        <AgentSection mode="Survival">
+            <Name>Odie</Name>
+            <AgentStart>
+                <Placement x="0.5" y="227.0" z="0.5"/>
+                <Inventory>
+                    <InventoryItem slot="0" type="wheat_seeds" quantity="'''+str(num_seeds)+'''"/>
+                </Inventory>
+            </AgentStart>
+            <AgentHandlers>
+                <ContinuousMovementCommands turnSpeedDegs="480"/>
+                <AbsoluteMovementCommands/>
+                <SimpleCraftCommands/>
+                <MissionQuitCommands/>
+                <InventoryCommands/>
+                <ObservationFromNearbyEntities>
+                    <Range name="entities" xrange="40" yrange="40" zrange="40"/>
+                </ObservationFromNearbyEntities>
+                <ObservationFromFullInventory/>
+                <AgentQuitFromCollectingItem>
+                    <Item type="rabbit_stew" description="Supper's Up!!"/>
+                </AgentQuitFromCollectingItem>
+            </AgentHandlers>
+        </AgentSection>
+
+    </Mission>'''
+
+# Create default Malmo objects:
+
+
+def teleport(agent_host, teleport_x, teleport_z):
+        """Directly teleport to a specific position."""
+        tp_command = "tp " + str(teleport_x)+ " 226 " + str(teleport_z)
+        agent_host.sendCommand(tp_command)
+        good_frame = False
+
+agent_host = MalmoPython.AgentHost()
+
+
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print('ERROR:',e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+
+my_mission = MalmoPython.MissionSpec(missionXML, True)
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission( my_mission, my_mission_record )
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print("Error starting mission:",e)
+            exit(1)
+        else:
+            time.sleep(2)
+
+# Loop until mission starts:
+print("Waiting for the mission to start ", end=' ')
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission running ", end=' ')
+
+dirt = set()
+rock = set()
+
+
+for i in range(-1,2):
+    for j in range(-1,2):
+        rock.add((i,j))
+
+for i in range(-3,4):
+    for j in range(-3,4):
+        dirt.add((i,j))
+
+for i in range(-1,2):
+    for j in range(-1,2):
+        dirt.remove((i,j))
+
+dirt = list(dirt)
+rock = list(rock)
+
+print("g")
+agent_host.sendCommand("pitch 0.5")
+
+for i in range(0, num_seeds):
+    x,z = dirt[i]
+    teleport(agent_host, x,z)
+    time.sleep(0.2);
+    agent_host.sendCommand("use 1")
+
+# Loop until mission ends:
+while world_state.is_mission_running:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission ended")
+# Mission has ended.
