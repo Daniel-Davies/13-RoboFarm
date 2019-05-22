274a4610aa761e7db3c7ef2ac83a3ffe90e6e59f
Brett Galkowski <bgalkows@uci.edu>
2019-05-22 11:14:06 -0700

    [Merge] Seed scoring + alternate MissionXML

--- Code/Simple.py
+++ Code/Simple.py
@@ -1,4 +1,3 @@
-
 from __future__ import print_function
 # ------------------------------------------------------------------------------------------------
 # Copyright (c) 2016 Microsoft Corporation
@@ -34,6 +33,7 @@ if sys.version_info[0] == 2:
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 else:
     import functools
+
     print = functools.partial(print, flush=True)
 
 # More interesting generator string: "3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"
@@ -76,7 +76,7 @@ missionXML_hell = '''<?xml version="1.0" encoding="UTF-8" ?>
             <AgentStart>
                 <Placement x="0.5" y="227.0" z="0.5"/>
                 <Inventory>
-                    <InventoryItem slot="0" type="wheat_seeds" quantity="'''+str(num_seeds)+'''"/>
+                    <InventoryItem slot="0" type="wheat_seeds" quantity="''' + str(num_seeds) + '''"/>
                 </Inventory>
             </AgentStart>
             <AgentHandlers>
@@ -133,7 +133,7 @@ missionXML_desert = '''<?xml version="1.0" encoding="UTF-8" ?>
             <AgentStart>
                 <Placement x="0.5" y="227.0" z="0.5"/>
                 <Inventory>
-                    <InventoryItem slot="0" type="wheat_seeds" quantity="'''+str(num_seeds)+'''"/>
+                    <InventoryItem slot="0" type="wheat_seeds" quantity="''' + str(num_seeds) + '''"/>
                 </Inventory>
             </AgentStart>
             <AgentHandlers>
@@ -154,86 +154,93 @@ missionXML_desert = '''<?xml version="1.0" encoding="UTF-8" ?>
 
     </Mission>'''
 
+
 # Create default Malmo objects:
 
 
 def teleport(agent_host, teleport_x, teleport_z):
-        """Directly teleport to a specific position."""
-        tp_command = "tp " + str(teleport_x)+ " 226 " + str(teleport_z)
-        agent_host.sendCommand(tp_command)
-        good_frame = False
+    """Directly teleport to a specific position."""
+    tp_command = "tp " + str(teleport_x) + " 226 " + str(teleport_z)
+    agent_host.sendCommand(tp_command)
+    good_frame = False
+
 
 def plant_random(mapsize, num_seeds):
     planting = []
     for i in range(num_seeds):
-        x = random.randint(-mapsize,mapsize)
-        y = random.randint(-mapsize,mapsize)
-        planting.append((x,y))
+        x = random.randint(-mapsize, mapsize)
+        y = random.randint(-mapsize, mapsize)
+        planting.append((x, y))
 
     return planting
 
 
-
 def initialise_planting_coords(mapsize, num_seeds):
     seed_locations = []
-    while(True):
-        x = random.randint(-mapsize,mapsize)
-        y = random.randint(-mapsize,mapsize)
-        if((x,y) not in seed_locations):
-            seed_locations.append((x,y))
+    while (True):
+        x = random.randint(-mapsize, mapsize)
+        y = random.randint(-mapsize, mapsize)
+        if ((x, y) not in seed_locations):
+            seed_locations.append((x, y))
         if (len(seed_locations) == num_seeds):
             break
     return seed_locations
 
+
 def breed(parent1, parent2):
-    parents = [parent1[0],parent2[0]]
-    x = random.randint(0,1)
-    y = random.randint(0,1)
+    parents = [parent1[0], parent2[0]]
+    x = random.randint(0, 1)
+    y = random.randint(0, 1)
 
     return ((parents[x])[0], (parents[y])[1])
 
+
 def cross_over(best):
-    #return a list of children
+    # return a list of children
     best_tuples = []
     for i in best:
         best_tuples.append(i[0])
     newchilren = []
     for i in range(len(best)):
-        parent1 = best[random.randint(0, len(best)-1)]
-        parent2 = best[random.randint(0,len(best)-1)]
+        parent1 = best[random.randint(0, len(best) - 1)]
+        parent2 = best[random.randint(0, len(best) - 1)]
         new_coord = breed(parent1, parent2)
-        while(True):
-            if(new_coord in best_tuples or new_coord in newchilren):
-                x,z = new_coord
+        while (True):
+            if (new_coord in best_tuples or new_coord in newchilren):
+                x, z = new_coord
                 x = x + 1
                 z = z - 1
-                new_coord = (x,z)
+                new_coord = (x, z)
             else:
 
                 newchilren.append(new_coord)
                 break
     return newchilren
 
+
 def mutate(coordinate):
-    x,y = coordinate
+    x, y = coordinate
     offsetX = random.randint(-3, 3)
     offsetY = random.randint(-3, 3)
     x = x + offsetX
     y = y + offsetY
-    return (x,y)
+    return (x, y)
+
 
 def select_elite_seeds(scores_tuples):
-    #return top 50%- they made it to the next round
+    # return top 50%- they made it to the next round
     top50 = len(scores_tuples) / 2
     return scores_tuples[0:int(top50)]
 
+
 def select_weaker_seeds(scores_tuples):
-    #return top 50%- they made it to the next round
+    # return top 50%- they made it to the next round
     top50 = len(scores_tuples) / 2
     return scores_tuples[int(top50):]
 
-def score_seeds(seed_locations,dirt, rock, water):
-    assert(len(water) > 0)
+
+def score_seeds(seed_locations, dirt, rock, water):
+    assert (len(water) > 0)
     scores = dict()
 
     for seed_tup in seed_locations:
@@ -244,7 +251,7 @@ def score_seeds(seed_locations,dirt, rock, water):
             # Calculate distance from each water coordinate and score based on minimum
             water_distances = []
             for water_tup in water:
-                water_distances.append( hypot(water_tup[0] - seed_tup[0], water_tup[1] - seed_tup[1]) )
+                water_distances.append(hypot(water_tup[0] - seed_tup[0], water_tup[1] - seed_tup[1]))
             water_dist = min(water_distances)
 
             max_distance = sqrt(200)  # sqrt(10^2 + 10^2), will vary based on map size
@@ -259,7 +266,7 @@ def score_seeds(seed_locations,dirt, rock, water):
     '''
     for i in range(len(seed_locations)):
         scores[seed_locations[i]] = 0
-    
+
     for f in range(len(seed_locations)):
         x,z = seed_locations[f]
 
@@ -271,26 +278,27 @@ def score_seeds(seed_locations,dirt, rock, water):
     print(scores)
     return scores.items()
 
-def getBestPlantingCoords(dirt,rock,water, num_seeds):
+
+def getBestPlantingCoords(dirt, rock, water, num_seeds):
     planting_coords = initialise_planting_coords(10, num_seeds)
 
-    #for now lets do 10 rounds
+    # for now lets do 10 rounds
     for i in range(10):
-        scores = score_seeds(planting_coords, dirt, rock, water) #as a 
+        scores = score_seeds(planting_coords, dirt, rock, water)  # as a
         planting_coords = []
-        scores = sorted(scores, key=lambda x:x[1])[::-1]
+        scores = sorted(scores, key=lambda x: x[1])[::-1]
 
         best = select_elite_seeds(scores)
 
         for i in best:
             planting_coords.append(i[0])
-        
+
         children = cross_over(best)
-  
+
         for i in children:
             planting_coords.append(i)
-        
-        mutation = random.randint(0, len(planting_coords)-1)
+
+        mutation = random.randint(0, len(planting_coords) - 1)
         print("----------------------------------")
         print("The index that is mutated is: " + str(mutation))
         print(planting_coords)
@@ -300,12 +308,13 @@ def getBestPlantingCoords(dirt,rock,water, num_seeds):
 
     return planting_coords
 
+
 agent_host = MalmoPython.AgentHost()
 
 try:
-    agent_host.parse( sys.argv )
+    agent_host.parse(sys.argv)
 except RuntimeError as e:
-    print('ERROR:',e)
+    print('ERROR:', e)
     print(agent_host.getUsage())
     exit(1)
 if agent_host.receivedArgument("help"):
@@ -319,11 +328,11 @@ my_mission_record = MalmoPython.MissionRecordSpec()
 max_retries = 3
 for retry in range(max_retries):
     try:
-        agent_host.startMission( my_mission, my_mission_record )
+        agent_host.startMission(my_mission, my_mission_record)
         break
     except RuntimeError as e:
         if retry == max_retries - 1:
-            print("Error starting mission:",e)
+            print("Error starting mission:", e)
             exit(1)
         else:
             time.sleep(2)
@@ -336,7 +345,7 @@ while not world_state.has_mission_begun:
     time.sleep(0.1)
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
-        print("Error:",error.text)
+        print("Error:", error.text)
 
 print()
 print("Mission running ", end=' ')
@@ -345,11 +354,11 @@ dirt = set()
 rock = set()
 water = set()
 
-for i in range(-10,11):
-    for j in range(-10,11):
-        dirt.add((i,j))
+for i in range(-10, 11):
+    for j in range(-10, 11):
+        dirt.add((i, j))
 
-water.add((0,0))
+water.add((0, 0))
 
 for i in water:
     dirt.remove(i)
@@ -363,13 +372,13 @@ agent_host.sendCommand("pitch 0.5")
 planting_coords = getBestPlantingCoords(dirt, rock, water, num_seeds)
 print(planting_coords)
 
-#planting_coords = plant_random(10,10)
+# planting_coords = plant_random(10,10)
 
 planted_indices = []
 for i in range(0, len(planting_coords)):
-    x,z = planting_coords[i]
+    x, z = planting_coords[i]
     print("planting: ", x, " ", z)
-    if((x,z) in rock):
+    if ((x, z) in rock):
         print("Hit rock")
     teleport(agent_host, x, z)
     time.sleep(2);
@@ -378,14 +387,13 @@ for i in range(0, len(planting_coords)):
 
 time.sleep(5)
 for i in planted_indices:
-    x,z = planting_coords[i]
+    x, z = planting_coords[i]
     teleport(agent_host, x, z)
     time.sleep(2);
     print("removing: ", x, " ", z)
     agent_host.sendCommand("attack 1")
     time.sleep(0.1)
     agent_host.sendCommand("attack 0")
-
 
 '''
 # Loop until mission ends:
@@ -397,9 +405,8 @@ while world_state.is_mission_running:
         print("Error:",error.text)
 '''
 
-
 print()
 print("Mission ended")
 # Mission has ended.
 agent_host.sendCommand("attack 0")
-#pitch up etc
+# pitch up etc
